fe5a6bae7ff453e52be31528f6b21c89e89e87e3
John Volk <j.volk@capstonedigital.com>
2013-09-30 15:29:38 -0500

    Straightening up.

--- resources/public/js/run.js
+++ resources/public/js/run.js
@@ -5,11 +5,8 @@ define([
     'controller/teams'],
 	function (angular)
 	{
-        //angular.bootstrap(document, ['benjals']);
-
         angular.element(document).ready(function()
         {
-            console.log('app ready');
             angular.bootstrap(document, ['benjals']);
 		});
 	});
