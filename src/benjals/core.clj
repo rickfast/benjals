253d2d4681affd11d82ad55cf14d1e98f226f098
John Volk <j.volk@capstonedigital.com>
2013-09-30 14:56:27 -0500

    Attempting routing "fix".

--- src/benjals/core.clj
+++ src/benjals/core.clj
@@ -11,7 +11,6 @@
 (defroutes routes
   (GET "/teams" [] {:body (all)})
   (POST "/teams" {body :body} {:body (create body)})
-  (GET "/" [] (resp/redirect "/index.html"))
   (route/resources "/"))
 
 (def app
